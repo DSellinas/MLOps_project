86aa95b203940df8cfbe516920d40022435a516b
Dionysios Sellinas <s242973@dtu.dk>
2026-01-12 18:18:02 +0100

    Add basic import and model forward tests

---
+++ tests/test_imports.py
@@ -0,0 +1,7 @@
+def test_lightning_module_import():
+    from credit_card_fraud_analysis.lightning_module import LitAutoEncoder
+    assert LitAutoEncoder is not None
+    
+    
+def test_train_lightning_import():
+    import credit_card_fraud_analysis.train_lightning
